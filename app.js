118431996c41a29b4ca587eaae896e438e02d2e4
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2015-06-25 15:47:53 -0500

    NODE ENV

--- app.js
+++ app.js
@@ -69,17 +69,17 @@ app.use(function(req, res, next) {
 // error handlers
 
 
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
+development error handler
+will print stacktrace
+if (app.get('env') === 'production') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)5;
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
 // production error handler
 // no stacktraces leaked to user
