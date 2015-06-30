a03a9c2bda22c422beee5079ecf7f964b4374be0
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2015-06-30 10:03:01 -0500

    Fixed database connection

--- db_helper.js
+++ db_helper.js
@@ -1,32 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
-var connection =  module.exports = require('mysql').createConnection({
+var db_config = {
+  connectionLimit : 10,
   host     : 'us-cdbr-iron-east-02.cleardb.net',
   user     : 'b420131646c183',
   password : '8f72cbf2',
   database : 'heroku_ed009e298388bba'
- });
+ };
 
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
 
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
+var pool =  module.exports = mysql.createPool(db_config);
+
+
+function handle_database(req,res) {
+    
+    pool.getConnection(function(err,connection){
+        if (err) {
+          connection.release();
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }        
+
+        connection.on('error', function(err) {      
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;     
+        });
   });
 }
 
-connection.connect();
+pool.getConnection();
